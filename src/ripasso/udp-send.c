3272bcc297534ebfe0da8d62219c5ca6d68094f3
Giacomo Ritucci <giacomo.ritucci@email.it>
2008-11-09 22:45:16 +0100

    upd-send: giusto lo scheletro del main

---
+++ src/ripasso/udp-send.c
@@ -0,0 +1,16 @@
+/*
+ * udp-send.c = simple UDP send test.
+ */
+
+#include <assert.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+
+int
+main (int argc, char **argv)
+{
+	return 0;
+}
